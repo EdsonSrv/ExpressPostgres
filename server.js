d95fac18a10232f91c9d4d119810b94cf57d7478
Edson Servin <edson@ciencias.unam.mx>
2019-10-31 13:31:07 -0600

    coding master Adding simple query

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ const client = new Client({
 
 start()
 async function start(){
-    await connect();
-   // const youtube = await readYoutube();
-   // console.log(youtube)
+    await connect()
+    const youtube = await readYoutube()
+    console.log(youtube)
 
-   // const successCreate = await createYoutube("Go to trader joes")
-   // console.log(`Creating was ${successCreate}`)
+    const successCreate = await createYoutube("Go to trader joes")
+    console.log(`Creating was ${successCreate}`)
 
    // const successDelete = await deleteYoutube(1)
    // console.log(`Deleting was ${successDelete}`)
@@ -23,10 +23,37 @@ async function start(){
 
 async function connect(){
   try{
-    await client.connect();
-    console.log(`Success`)
+    await client.connect()
+    console.log(`Connection Success`)
   }
   catch(e){
     console.log(`Failed to connect ${e}`)
+  }
+}
+
+async function readYoutube(){
+  try {
+    const result = await client.query("select id, title, description from videos")
+    return result.rows
+  } catch (error) {
+    return []    
+  }
+}
+
+async function createYoutube(text){
+  try {
+    await client.query("insert into videos (id, title, description) values (1, 'hola mundo', 'description text')")
+    return true
+  } catch (error) {
+    return false
+  }  
+}
+
+async function deleteYoutube(id){
+  try {
+    await client.query("delete from videos where id = $1",[id])
+    return true
+  } catch (error) {
+    return false
   }
 }
